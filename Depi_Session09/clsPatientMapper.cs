2424969aca7e6d504181ff71c4c6158911fd22f4
Mohamed Ahmed <ebaidmedo101@gmail.com>
2026-09-23 23:05:12 +0300

    Add PatientMapper static class for mapping PatientDto

---
+++ Depi_Session09/clsPatientMapper.cs
@@ -0,0 +1,11 @@
+namespace DEPI_Session09
+{
+    // Q4) Static class = cannot be instantiated, contains only static members.
+    public static class PatientMapper
+    {
+        public static PatientDto MapFromModelToDto(Patient patient)
+        {
+            return new PatientDto(patient.Id, patient.FullName, patient.PhoneNumber);
+        }
+    }
+}
